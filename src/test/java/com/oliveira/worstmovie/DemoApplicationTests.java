0a7c7880da4b363e8d661fbb94d97ead902128c0
Lucas Oliveira <lcosbr@live.com>
2022-10-28 11:53:32 -0300

    First Commit

---
+++ src/test/java/com/oliveira/worstmovie/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.worstmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
